137ed1300b024fbcb9ce4a5afec0de1c790d0c8a
Ashley-Pritchard <ashley.pritchard@ouh.nhs.uk>
2019-04-12 15:45:35 +0000

    fixed datetime issue, added tests throughout and added more user input stages

--- hg38_to_Hg19_batch.py
+++ hg38_to_Hg19_batch.py
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-#note - run from the command line?
-#note - add some tests 
-
-#note - pyliftover output - '+' '-' and long number?
-#note - del and dup issue - reference position or genomic coordinate used? 
-
+#import relevant libraries
 import psycopg2
 import pandas as pd
 import sys
@@ -20,9 +15,7 @@ import glob
 from collections import defaultdict 
 from pyliftover import LiftOver
 
-update_time = input("Please input update date in format yyy-mm-dd: ")
-
-#specify which assembly lifting from and to
+#specify which genome assembly lifting from and to
 lo = LiftOver('hg38', 'Hg19')
 
 #connect to databse 
@@ -35,8 +28,40 @@ conn = psycopg2.connect(host='localhost', database=db_name, user=username, passw
 
 cur = conn.cursor()
 
+#if csv files output from this script already exist in the current directory, they will be deleted. Check this is okay with the user first, exit the script if not.
+user_response = input('Running this script will delete any gel_id.csv or Gel2MDT_Export_*_Mutation Report.csv files stored in this directory. Are you sure you want to continue? Please answer y or n: ')
+
+if user_response == 'n':
+	sys.exit()
+elif user_response != 'y':
+	print('This is not a valid input, please run the script agiain if you wish to continue')
+	sys.exit()
+
+#check if csv files output from this script already exist in the current directory, and delete if so - otherwise, will cause problems downstream
+if os.path.exists('gel_id.csv'):
+	os.remove('gel_id.csv')
+existing_files = glob.glob('Gel2MDT_Export_*_MutationReport.csv')
+for item in existing_files:
+	if os.path.exists(item):
+		os.remove(item)
+
+#ask for input of import date into the Gel2MDT database via the command line 
+update_time = input("Please input update date in format yyyy-mm-dd: ")
+
+#check that the user has input the date in the correct format. Error if not. 
+def validate(update_time):
+	try:
+		datetime.datetime.strptime(update_time, '%Y-%m-%d')
+	except ValueError:
+		raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+
+#call fucntion
+validate(update_time)
+
+#create a csv file with a list of gel ids uploaded on the date input by the user from the Gel2MDT database.
 def get_list(update_time):
 
+	#lift the gel ids from the database
 	column_head = ["Gel_id"]
 	cur.execute('''
 	SELECT "Proband"."gel_id"
@@ -51,7 +76,7 @@ def get_list(update_time):
 	WHERE "ListUpdate"."update_time"::date = %s AND "ListUpdate"."cases_added" >0 AND "ListUpdate"."sample_type" = 'raredisease'
 	''', (update_time,))	
 	
-
+	#save the gel ids to a csv file
 	rows = cur.fetchall()
 	filename = 'gel_id.csv'
 	with open(filename, 'w') as file:
@@ -60,22 +85,35 @@ def get_list(update_time):
 		for row in rows:
 			writer.writerow(row)
 
+#call function
 get_list(update_time)
 
+#read in the 'gel_id.csv' file, set the id as the index
 df_gel = pd.read_csv('gel_id.csv')
-df_gel.set_index('Gel_id', inplace=True)
+
+#if the database is empty, inform the user that no updates were made on this date and remove the file.
+if df_gel.empty:
+	print('There were no updates on this date, please amend your search.')
+	os.remove('gel_id.csv')
+	sys.exit()
+#else inform the user that the gel ids have been lifted
+else:
+	print('Gel ids have been lifted from the Gel2MDT database for this date. A csv file for each will now be created.')
+
+#assign each of the gel ids collected to a variable so a csv file can be created for each of them
 for index, row in df_gel.iterrows():
-	gel_id = str(index)
+	gel_id = str(row[0])	
 
 	#name csv file based on gel_id
 	csv_file = 'Gel2MDT_Export_%s_MutationReport.csv' % (gel_id,)
 
-	#populate csv file from database
+	#populate csv file from the database
 	def variant_pull(gel_id):
-
-	#specify headers of output csv file
+		
+		#specify headers of output csv file
 		column_head = ['hg38 Reference Position', 'Gene', 'Reference Sequence', 'Alternative Sequence', 'Transcript', 'Chr', 'Mutation Call', 'Amino Acid Change', 'Genotype', 'Genomic Coordinate', 'Alamut']
 
+		#pull relevant data out of the database for each gel id
 		cur.execute('''
 		SELECT "Variant"."position", "Gene"."hgnc_name", "Variant"."reference", "Variant"."alternate", "Transcript"."name", "Variant"."chromosome", "TranscriptVariant"."hgvs_c", "TranscriptVariant"."hgvs_p", "ProbandVariant"."zygosity", "TranscriptVariant"."hgvs_g", "TranscriptVariant"."hgvs_g"
 		FROM "Proband"
@@ -90,7 +128,7 @@ for index, row in df_gel.iterrows():
 		WHERE "Proband"."gel_id" = %s
 		''', (gel_id,))
 
-		#write csv file
+		#write csv file for each gel id
 		rows = cur.fetchall()
 		filename = 'Gel2MDT_Export_%s_MutationReport.csv' % (gel_id,)
 		with open(filename, 'w') as file:
@@ -99,17 +137,29 @@ for index, row in df_gel.iterrows():
 			for row in rows:
 				writer.writerow(row)
 	
+	#call function
 	variant_pull(gel_id)
 
+	#some gel ids return empty csv files - inform user and delete files
 	def delete_csv(csv_file):
 		df = pd.read_csv(csv_file)
 		if df['hg38 Reference Position'].isnull().sum().sum() >= 1:
-			print(csv_file + ' is empty')
+			print(csv_file + ' is empty and has been deleted')
 			os.remove(csv_file)
 
+	#call function
 	delete_csv(csv_file)
 
+#assign each of the csv files to a variable so LiftOver can be carried out for each of them
 input_file_list = glob.glob('Gel2MDT_Export_*_MutationReport.csv')
+
+#inform user how many csv files have been created
+print(str(len(input_file_list)) + ' mutation reports have been created')
+
+#check that there are non-empty csv files for LiftOver, exit if not and inform the user
+if len(input_file_list) == 0:
+	print('All gel ids returned empty files')
+	sys.exit()
 
 #liftover of 'hg38 Reference Position'
 def lift_over(input_file_list):
@@ -121,7 +171,7 @@ def lift_over(input_file_list):
 		#tool requires input in format 'chr1	112345678' so add 'chr' as string before chromosome no. in 'Chr' field
 		df['Chr'] = 'chr' + df['Chr'].astype(str)
 
-		#tool requires Mitochondrial genome in format 'chrM', ours is 'chrMT' - this is the only instance a T would end, so it
+		#tool requires Mitochondrial genome in format 'chrM', ours is 'chrMT' - this is the only instance a T would end the field, so strip it
 		df['Chr'] = df['Chr'].str.rstrip('T')
 	
 		#create blank field for Hg19 conversion position
@@ -138,10 +188,14 @@ def lift_over(input_file_list):
 		df['Hg19'] = Hg19
 		#overwrite csv file
 		df.to_csv(input_file, sep=',')
-		
+
+	#inform user of stage
+	print('LiftOver of Reference Position complete')
+
+#call function		
 lift_over(input_file_list)
 
-#liftover output is '[('chr1', 12345678, '+', 12345678910)] - need to extra the coordinate
+#liftover output is '[('chr1', 12345678, '+', 12345678910)] - need to extract the coordinate
 def reformat_lift_over(input_file_list):
 
 	#read in batch of csv files as pandas dataframe
@@ -150,7 +204,7 @@ def reformat_lift_over(input_file_list):
 		#in the liftover function, pandas assigned an index - assign the index to this column again to prevent duplication
 		df.set_index('Unnamed: 0', inplace=True)
 
-		#split the Hg19 field into 4 on ',' ( [('chr1' / 12345678 / '+' / 12345678910)] ) and save index 1 as 'Reference Position' 
+		#split the Hg19 field into 4 on ',' ( [('chr1' / 12345678 / '+' / 12345678910)] ) and save index 1 (zero based) as 'Reference Position' 
 		df['Reference Position'] = df['Hg19'].str.split(',', n=4, expand=True)[1]
 
 		#drop unecessary column
@@ -158,11 +212,11 @@ def reformat_lift_over(input_file_list):
 
 		#overwrite csv file
 		df.to_csv(input_file, sep=',')
-	
+
+#call function
 reformat_lift_over(input_file_list)
 	
-#for del / dup need to do a liftover of the 'Genomic Coordinate' which is in the format '1:g.12345678_12345680del'
-#first need to extract the coordinates
+#for del / dup / ins need to do a liftover of the 'Genomic Coordinate' which is in the format '1:g.12345678_12345680del' - first need to extract the coordinates
 def extract_genomic_coord(input_file_list):
 
 	#read in batch of csv files as pandas dataframe
@@ -174,7 +228,7 @@ def extract_genomic_coord(input_file_list):
 		#split 'Genomic Coordinate' into 2 on '.' (1:g / 12345678_12345680del) and save index 1 as 'GC' (genomic coordinate)
 		df['GC'] = df['Genomic Coordinate'].str.split('.', n=2, expand=True)[1]
 	
-		# split remainder on '_' and all genomic change possibilities into the two coordinates and save as new dataframe
+		# split remainder on '_' and all possible genomic changes into the two coordinates and save as new dataframe
 		df2 = df['GC'].str.split('_|T|G|A|C|dup|del|ins|>', n=2, expand=True)
 		#assign each coordinate to a field
 		df['First Coordinate'] = df2[0]
@@ -187,10 +241,11 @@ def extract_genomic_coord(input_file_list):
 
 		#overwrite csv file
 		df.to_csv(input_file, sep=',')
-
+	
+#call function
 extract_genomic_coord(input_file_list)
 
-#can now do liftover
+#can now do liftover of the two genomic coordinates
 def lift_over_genomic_coord(input_file_list):
 
 	#read in batch of csv files as pandas dataframe
@@ -226,10 +281,14 @@ def lift_over_genomic_coord(input_file_list):
 
 		#overwrite csv file
 		df.to_csv(input_file, sep=',')
-	
+
+	#inform user of stage
+	print('LiftOver of Genomic Coordinate complete')
+
+#call function
 lift_over_genomic_coord(input_file_list)
 
-#liftover output is '[('chr1', 12345678, '+', 12345678910)] - need to extract the coordinate
+#as above, liftover output is '[('chr1', 12345678, '+', 12345678910)] - need to extract the coordinate
 def reformat_genomic_lift_over(input_file_list):
 
 	#read in batch of csv files as pandas dataframe
@@ -240,19 +299,21 @@ def reformat_genomic_lift_over(input_file_list):
 
 		#split 'First Coordinate LiftOver' into 4 on ',' ( [('chr1' / 12345678 / '+' / 12345678910)] ) and save index 1 as 'First Referenece Position'
 		df['First Reference Position'] = df['First Coordinate LiftOver'].str.split(',', n=4, expand=True)[1]
-		#do the same with the 'Second Coordinate LiftOver'
+		#do the same with the 'Second Coordinate LiftOver'. Also split on '[]' as this is the output for the rows without a second coordinate to liftover. 
 		df['Second Reference Position'] = df['Second Coordinate LiftOver'].str.split(',|\[|]', n=4, expand=True)[2]
 		#use split to remove the '.0' that is added to the second reference position
 		df['Second Reference Position'] = df['Second Reference Position'].str.split('.', n=2, expand=True)[0]
 
-		#drop all unneccasary columns 
+		#drop unneccasary columns 
 		df.drop(columns = ['First Coordinate LiftOver', 'Second Coordinate LiftOver'], inplace=True)
 
 		#overwrite csv file
 		df.to_csv(input_file, sep=',')
-
+	
+#call function
 reformat_genomic_lift_over(input_file_list)
 
+#reformat the 'Genomic Coordinate' field to contain the LiftOver output 'Chr1.hg19:g.12345678(_12345689)
 def update_genomic_coord(input_file_list):
 
 	#read in batch of csv files as pandas dataframe
@@ -279,8 +340,10 @@ def update_genomic_coord(input_file_list):
 		#overwrite csv file
 		df.to_csv(input_file, sep=',')
 
+#call function
 update_genomic_coord(input_file_list)
 
+#reformat the 'Alamut' field to contain the LiftOver output '1:g.12345678G>A / 1:g.12345678_12345689del
 def update_alamut_coord(input_file_list):
 
 	#read in batch of csv files as pandas dataframe
@@ -300,11 +363,14 @@ def update_alamut_coord(input_file_list):
 		df['Concat Coordinates'] = df['Concat Coordinates'].str.split('.', n=2, expand=True)[0]
 		#strip the '_' from the missence variants which do not have a second reference position
 		df['Concat Coordinates'] = df['Concat Coordinates'].str.rstrip('_')
-	
+		
+		#create a field with the original coordinates in the format 1:g.12345678
 		df['Delete'] = df['Chrom.g'] + '.' + df['Concat Coordinates'].astype(str)
 
+		#replace this field from the 'Genomic Change' field to leave only the genomic change
 		df['Genomic Change'] = df['Alamut'].replace(to_replace= df['Delete'], value='', regex=True)
 
+		#reformat the 'Alamut' field to the LiftOver output 1:g.12345678A>G / 1:g.12345678_12345689del
 		df['Alamut'] = df['Chrom.g'].astype(str) + '.' + df['Concat Reference Positions'].astype(str) + df['Genomic Change'].astype(str)
 
 		#drop unnecessary columns
@@ -313,8 +379,13 @@ def update_alamut_coord(input_file_list):
 		#overwrite csv file 
 		df.to_csv(input_file, sep=',')
 
+	#inform user of stage
+	print('Alamut input updated')
+
+#call function
 update_alamut_coord(input_file_list)
 
+#reorder the columns to match the csv file format used downstream in the workflow
 def reorder(input_file_list):
 	
 	#read in batch of csv files as pandas dataframe
@@ -331,8 +402,13 @@ def reorder(input_file_list):
 		#overwrite csv, don't save the index
 		df.to_csv(input_file, sep=',', index=False)
 
+	#inform user of stage
+	print('Mutation reports formated for output')
+
+#call function
 reorder(input_file_list)
 
+#add the date and time to the csv file for tracability
 def add_date_time(input_file_list):
 
 	#set the date variable to the current date and time 
@@ -344,24 +420,15 @@ def add_date_time(input_file_list):
 			r = csv.reader(f)
 			data = [line for line in r]
 	
-	#write '#Export: todays date and time' in the form '%c' - inbuilt python for local appropriate date and time representation
-	for input_file in input_file_list:
-		with open(input_file,'w',newline='') as f:
-			w = csv.writer(f)
-			w.writerow(['#Export date: ' + date.strftime('%c') + '\n'])
-			w.writerows(data)
-			f.close()
+			#write '#Export: todays date and time' in the form '%c' - inbuilt python for local appropriate date and time representation
+			with open(input_file,'w',newline='') as f:
+				w = csv.writer(f)
+				w.writerow(['#Export date: ' + date.strftime('%c') + '\n'])
+				w.writerows(data)
+				f.close()
+	#inform user of stage
+	print('Mutation reports are ready')
 
-add_date_time(input_file_list)	
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
+#call function
+add_date_time(input_file_list)		
 
